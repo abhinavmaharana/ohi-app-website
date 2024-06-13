527b0d7e483bcc995b2507e7890226702aee6206
abhinavmaharana <abhinavmaharana.004@gmail.com>
2024-06-13 11:31:24 +0530

    fixed

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,12 @@ import Navbar from "./components/shared/navigation/Navbar";
 import HomePage from "./pages/home";
 import PrivacyPage from "./pages/privacy-page";
 import TermsPage from "./pages/terms-page";
+import ScrollToTop from "./hook/ScrollToTop";
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <div>
         <div className="px-[20px] pt-[10px] md:px-[50px] md:pt-[25px]">
           <Navbar />
