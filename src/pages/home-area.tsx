03bca7d021962dfff9d7428d0e65a80105473b66
abhinavmaharana <abhinavmaharana.004@gmail.com>
2024-10-30 12:56:16 +0530

    fix

--- src/pages/home-area.tsx
+++ src/pages/home-area.tsx
@@ -12,6 +12,7 @@ export default function HomeAreaPage() {
         <div>
             <div className=''>
               <HomeNew />
+              
             </div>
             <div className=''>
             <Renowned />
