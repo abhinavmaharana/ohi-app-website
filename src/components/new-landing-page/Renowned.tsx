92ebe7d4311c87ca23a073e23e510ef89d913d0a
abhinavmaharana <abhinavmaharana.004@gmail.com>
2024-11-05 07:57:39 +0530

    🐛 responsive fixes

--- src/components/new-landing-page/Renowned.tsx
+++ src/components/new-landing-page/Renowned.tsx
@@ -11,13 +11,13 @@ import Badge7 from '@/assets/badge7.png'
 export default function Renowned() {
   return (
     <div id="earn-new" className='background-renowned'>
-                <div className='mt-[50px] lg:mt-0'>
+                <div className='mt-[50px] md:mt-[10px] lg:mt-0'>
                 <div className="px-[20px] lg:px-[50px] text-center pt-[20px] lg:pt-[30px]">
-                    <h1 className="items-center text-[32px]  lg:text-[48px] text-[#3BA0FF]  lg:mt-[250px] lg:mb-[20px] leading-normal  poppins-bold">Saga of FAME: Feel the thrill of being recognized wherever you go!</h1>
+                    <h1 className="items-center text-[32px]  lg:text-[48px] text-[#3BA0FF] md:mt-[230px]  lg:mt-[250px] lg:mb-[20px] leading-normal  poppins-bold">Saga of FAME: Feel the thrill of being recognized wherever you go!</h1>
                 </div>
                 <div className='flex flex-col lg:flex-row justify-between'>
                     <div className='px-[20px] lg:px-[50px]'>
-                        <ul className='grid grid-cols-1 lg:grid-cols-2 items-center lg:mt-[50px] lg:gap-x-48 xl:gap-4'>
+                        <ul className='grid grid-cols-1 md:grid-cols-2 items-center lg:mt-[50px] md:space-y-1 lg:gap-x-48 xl:gap-4'>
                             <li>
                                 <div className='flex items-center lg:w-[300px] xl:w-auto'>
                                     <img src={Badge1} className='w-[140px] h-[140px]' alt="" />
@@ -63,12 +63,12 @@ export default function Renowned() {
                             </li>
                         </ul>
                     </div>
-                    <div className=' lg:flex flex-col'>
+                    <div className='lg:flex flex-col md:-mt-[100px] lg:mt-0'>
                         {/* Image */}
                         <div>
-                            <img src={HandImg} className='lg:h-[500px] lg:ml-56'/>
+                            <img src={HandImg} className='md:h-[400px] lg:h-[500px] md:ml-[565px] lg:ml-56'/>
                         </div>
-                        <div className='lg:w-[634px] ml-[34px] lg:mb-[50px]  rounded-l-[29.87px] bg-[#EECA07] text-white'>
+                        <div className='lg:w-[634px] ml-[34px]  lg:mb-[50px] rounded-l-[29.87px] bg-[#EECA07] text-white'>
                             <p className=' lg:text-[20px] pl-6 pr-20 py-6 poppins-medium'>
                             Make your presence count at every 'Social Hotspot' you visit. Let the people get notified about your Influential Presence at every Café/Airport you visit with the 'Real Life Fame Tick'!
                             </p>
